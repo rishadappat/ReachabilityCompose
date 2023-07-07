6f856a554780549b3407e61b21a520d954d3fded
Rishad Appat <rishadappat@gmail.com>
2023-07-07 09:09:46 +0400

    Fixed gradle

--- reachability/src/main/java/com/appat/reachability/Connectivity.kt
+++ reachability/src/main/java/com/appat/reachability/Connectivity.kt
@@ -1,6 +1,5 @@
 package com.appat.reachability
 
-import android.app.Activity
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.Network
@@ -39,7 +38,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
