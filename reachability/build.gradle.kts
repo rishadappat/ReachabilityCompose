cda597069f66b06ee85e8f52e09a6ef204fcb4e9
Rishad Appat <rishadappat@gmail.com>
2023-09-20 14:34:25 +0400

    Updated libraries

--- reachability/build.gradle.kts
+++ reachability/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.activity:activity-compose:1.7.2")
     val composeBom = platform("androidx.compose:compose-bom:2023.06.01")
-    implementation(composeBom)
+    implementation("androidx.compose:compose-bom:2023.09.00")
     implementation("androidx.compose.material3:material3:1.1.1")
     implementation ("androidx.compose.ui:ui")
 }
